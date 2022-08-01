97f00590e246d399f6e94ca969301a15ce9d841c
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-08-01 15:00:26 -0300

    feat:adding initial project structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './GlobalStyle.css'
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login'
+
 
 function App() {
   return (
-    <div>
-      <header>
-        <p>Olá </p>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header/>
+          <Routes> 
+            <Route path='/' element={<Home/>}/>
+            <Route path='/login' element={<Login/>}/>
+          </Routes>
+          <Footer/>
+      </BrowserRouter>    
+    </>  
   );
 }
 
