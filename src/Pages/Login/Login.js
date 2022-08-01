ec3bbeb1c630665d1885cdf37b6cf43ee3baa9ff
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-08-01 16:37:45 -0300

    feat:adding class as tags and building style login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+import styles from "./Login.module.css"
+import LoginCreate from '../../Pages/Login/LoginCreate'
+import LoginForm from '../../Pages/Login/LoginForm'
+import LoginLost from '../../Pages/Login/LoginLost'
+import LoginReset from '../../Pages/Login/LoginReset'
+
 
  const Login = () => {
   return (
-    <div>Login</div>
+    <section  className={styles.login} >
+      <div className={styles.forms}>
+        <Routes>
+          <Route path='/' element={<LoginForm/>} />
+          <Route path='/criar' element={<LoginCreate/>} />
+          <Route path='/perdeu' element={<LoginLost/>} />
+          <Route path='/resetar' element={<LoginReset/>} />
+        </Routes>      
+      </div>
+    </section>
   )
 }
 export default Login
