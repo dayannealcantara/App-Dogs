1c6013b2f0f63b3929b842a8381b463558e13ecc
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-08-05 15:41:41 -0300

    feat:fixing routes, adding context for login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,20 +1,25 @@
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route, Navigate} from 'react-router-dom'
 import styles from "./Login.module.css"
 import LoginCreate from '../../Pages/Login/LoginCreate'
 import LoginForm from '../../Pages/Login/LoginForm'
 import LoginLost from '../../Pages/Login/LoginLost'
 import LoginReset from '../../Pages/Login/LoginReset'
+import { useContext } from 'react'
+import { UserContext } from '../../UserContext'
 
 
  const Login = () => {
+   const  {login} = useContext(UserContext);
+   if (login === true) return <Navigate to='/conta'/>
   return (
     <section  className={styles.login} >
       <div className={styles.forms}>
+        
         <Routes>
           <Route path='/' element={<LoginForm/>} />
-          <Route path='/criar' element={<LoginCreate/>} />
-          <Route path='/perdeu' element={<LoginLost/>} />
-          <Route path='/resetar' element={<LoginReset/>} />
+          <Route path='criar' element={<LoginCreate/>} />
+          <Route path='perdeu' element={<LoginLost/>} />
+          <Route path='resetar' element={<LoginReset/>} />
         </Routes>      
       </div>
     </section>
