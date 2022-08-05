a0f4caccd19479c6ea480c40fb353d910d659c1e
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-08-05 15:44:16 -0300

    feat: adding create login page

--- src/Pages/Login/LoginCreate.js
+++ src/Pages/Login/LoginCreate.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { useContext } from 'react';
+import { USER_GET } from '../../api';
+import Input from '../../Components/Form/Input';
+import Button from '../../Components/Form/Button';
+import Error from '../../Components/Helper/Error';
+import useForm from '../../Hooks/useForms'
+import { UserContext } from '../../UserContext';
+import useFetch from '../../Hooks/useFetch'
 
  const LoginCreate = () => {
+   const username = useForm();
+   const email = useForm('email')
+   const password = useForm();
+   const { userLogin} = useContext(UserContext);
+   const { loading, error, request} = useFetch()
+   
+   async function handleSubmit(e) {
+     e.preventDefault();
+     const { url, options} = USER_GET({
+       username: username.value,
+       email:email.value,
+       password:password.value,
+     });
+     const response = await request(url, options);
+     if(response.ok) userLogin(username.value, password.value);
+   }
   return (
-    <div>Login Criar</div>
+   <section className="animeLeft">
+     <h1 className="title">Cadastre-se</h1>
+     <form onSubmit={handleSubmit}>
+       <Input label='Usuário' type='text' name='username' {...username}/>
+       <Input label='Email' type='email' name='email' {...email}/>
+       <Input label='Senha' type='password' name='password' {...password}/>
+       {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error} />
+     </form>
+   </section>
   )
 }
 export default LoginCreate
