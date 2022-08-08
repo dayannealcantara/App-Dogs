29a6e6701048270a6ad5748920f04eb3422c18ee
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-08-08 09:27:07 -0300

    fix:correcting route name

--- src/UserContext.js
+++ src/UserContext.js
@@ -42,7 +42,7 @@ export const UserStroge =({children}) =>{
     const {token} = await tokenRes.json();
     window.localStorage.setItem('token', token);
      await getUser(token);
-     navigate('/conts');
+     navigate('/conta');
   } catch (err) {
     setError(err.message);
     setLogin(false)
