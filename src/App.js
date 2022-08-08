12c07aecfeba1d3dc9e29a7515960cf2ef4f0458
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-08-08 09:26:43 -0300

    feat:private route creation

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login'
+import { UserStroge } from './UserContext';
+import User from './Pages/User/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header/>
+      <UserStroge>
+      <Header/>
           <Routes> 
             <Route path='/' element={<Home/>}/>
-            <Route path='/login' element={<Login/>}/>
+            <Route path='/login/*' element={<Login/>}/>   
+          <Route path='/conta/*' element={<ProtectedRoute><User/></ProtectedRoute>}/>              
           </Routes>
           <Footer/>
+      </UserStroge>        
       </BrowserRouter>    
     </>  
   );
