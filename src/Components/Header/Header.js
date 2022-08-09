31ce82904933197d99ba1db47fba31af19f1a697
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-08-09 13:46:22 -0300

    feat: important userContext for login

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,13 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
 import { ReactComponent as Dogs } from '../../Assets/dogs.svg'
+import { useContext } from 'react'
+import { UserContext } from '../../UserContext'
 
  const Header = () => {
+   const {data} = useContext(UserContext)
   return (
     <div className={styles.header}>
       <nav className={ `${styles.nav} container`}>
         <Link to='/' className={styles.logo}><Dogs/></Link>
-        <Link to='/login' className={styles.login}>Login / Criar</Link>
+        {data ? (
+           <Link className={styles.login} to="/conta">
+           {data.nome}
+         </Link>
+        ) : (
+          <Link to='/login' className={styles.login}>Login / Criar</Link>
+        )}      
       </nav>
     </div>
   )
